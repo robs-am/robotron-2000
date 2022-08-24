02169c58d824f6bcdca5a8cda2b012a8c4f2819f
robs-am <roberta.amaro89@gmail.com>
2022-08-24 20:07:49 -0300

    add desafio aluraTintas

---
+++ js/datAtributes.js
@@ -0,0 +1,10 @@
+function mudaCores(elementos){
+    var cores = elementos.getAttribute("data-cor");
+    var tipoElemento = elementos.getAttribute("data-tipo");
+  }
+
+
+function mudaCores(elementos){
+    var cores = elementos.getAttribute("cor");
+    var tipoElemento = elementos.getAttribute("tipo");
+  }
--- js/main.js
+++ js/main.js
@@ -18,13 +18,14 @@ dizOi("Roberta") */
 const subtrair = document.querySelector('#subtrair')
 const somar = document.querySelector('#somar')
 const braco = document.querySelector('#braco')
-const controle = document.querySelectorAll('.controle-ajuste') // variável que buscará o nó para manipular todos os controles. Nós são arrays e o melhor método de função para arrays é a foreach:
+const controle = document.querySelectorAll('[data-controle]') // variável que buscará o nó para manipular todos os controles. Nós são arrays e o melhor método de função para arrays é a foreach:
 
 
 
 controle.forEach( (elemento) => {
     elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.textContent)
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+        
     })         
 })
 
@@ -32,17 +33,22 @@ controle.forEach( (elemento) => {
 //depois adicionamos o addEventListener
 //por último adicionamos o evento na funçao anonima para que o console nos informe o que esta acontecendo quando é clicado e posteriormente target
 //adicionamos textContext para o console entender o somar e subtrair
+// troca-se a classe .controle-ajuste pelo atributo data-controle
 
 
    
 
 
-function manipulaDados(operacao) {          //terceiriza a validação em uma outra função para evitar repeti-la. Trazendo assim toda a logica de manipulação de dados para um único lugar. Como consequencia, agora a  unica responsabilidade do Eventlistener será a função click//
-    
+function manipulaDados(operacao, controle) {          
+    //terceiriza a validação em uma outra função para evitar repeti-la. Trazendo assim toda a logica de manipulação de dados para um único lugar. Como consequencia, agora a  unica responsabilidade do Eventlistener será a função click//
+        const peca = controle.querySelector('[data-contador]')
         if(operacao === "-") {
-            braco.value = parseInt(braco.value) -1
+            peca.value = parseInt(peca.value) -1
         } else {
-            braco.value = parseInt(braco.value) +1
+            peca.value = parseInt(peca.value) +1
         }    
     
     }
+
+    //trocamos o id braco pela classe controle-contador para  conseguirmos controlar todo o robo.
+    //No metodo forEach nao queremos buscar o controle-contador do documento inteiro e sim apenas do controle, então precisamos passar isso para o método atraves do parametro evento.target.parentNode na funcao manipulaDados
