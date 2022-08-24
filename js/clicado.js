4650b8bfc22d3f04eb592033f57033362945819e
robs-am <roberta.amaro89@gmail.com>
2022-08-23 22:29:27 -0300

    add manipulacao bracos robo

---
+++ js/clicado.js
@@ -0,0 +1,14 @@
+const button = document.querySelector("#calcular")
+
+calcular.addEventListener("click", function() {
+    console.log("Fui clicado.")
+})
+
+//ou//
+
+const calculo = document.querySelector('#calcular');
+const resultado = document.querySelector('.resultado');
+
+calculo.addEventListener('click', () => {
+    resultado.innerHTML = 'Fui clicado!'
+})
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const robotron = document.querySelector("#robotron") // usamos uma const para guardar um elemento. a const é uma variavel que nao pode ser alterada
+/* const robotron = document.querySelector("#robotron") // usamos uma const para guardar um elemento. a const é uma variavel que nao pode ser alterada
 
 robotron.addEventListener("click", dizOi) // usamos a variável dentro do escutador de eventos
 
@@ -6,18 +6,29 @@ robotron.addEventListener("click", function() { //função anonima só sera exec
     console.log("Cliquei no Robô.")
 })
 
-function dizOi() {     
-    console.log("oi")
+function dizOi(nome) {     
+    console.log(nome)
     console.log("Bem vindo ao Robotron 2000")
 }
+dizOi("Roberta") */
 
-/* const subtrair = document.querySelector("#subtrair")
-const somar = document.querySelector("#somar")
-const braco = document.querySelector("#braco")
+const bracoMais = document.querySelector('#somar')
+const bracoMenos = document.querySelector('#subtrair')
+const bracoValor = document.querySelector('#braco')
 
+bracoMais.addEventListener('click', () => {
+    braco.value++;
+})
 
+bracoMenos.addEventListener('click', () => {
+    if(braco.value > 0) {
+        braco.value--; } 
+    else {
+        alert("Não é possivel ter um valor negativo de peças.")
+    }
+})
 
-controle.forEach( (elemento) => {
+/* controle.forEach( (elemento) => {
     elemento.addEventListener("click", (evento) => {
         manipulaDados(evento.target.text)
     })
@@ -25,9 +36,9 @@ controle.forEach( (elemento) => {
 
 const controle = document.querySelectorAll(".controle-ajuste")
 
-somar.addEventListener("click", (evento) => {manipulaDados("somar")})
+somar.addEventListener("click", (evento) => {manipulaDados("somar")}) //toda vez que clicar no somar +1
 
-subtrair.addEventListener("click", (evento) => {manipulaDados("subtrair")})
+subtrair.addEventListener("click", (evento) => {manipulaDados("subtrair")}) //toda vez que clicar no subtrair -1
 
 function manipulaDados(operacao) {
     if(operacao === "-") {
@@ -36,4 +47,4 @@ function manipulaDados(operacao) {
         braco.value = parseInt(braco.value) + 1
     }
     
-} */
+}  */
